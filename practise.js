df85db14ea4b2899a7c58514c50c1e1e5773685a
sinan-haque <145665893+sinan-haque@users.noreply.github.com>
2024-06-12 09:26:44 +0600

    Add files via upload

--- practise.js
+++ practise.js
@@ -1,8 +1,4 @@
-const a=10;
-const b=10;
-if(a==b){
-    console.log('toi parsos')
-}
+// const a=12;
+// const b="12";
+// console.log(a+b)
 
-
-// (==) shudhu value dekhbe  and  (===) value and data 2 tai e dekhbe
